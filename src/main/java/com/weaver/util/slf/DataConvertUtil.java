a14338d7d78b126f7fc3dc0c75d2247941f4e667
maple <1326808745@qq.com>
2023-11-24 11:12:48 +0800

    [feat]新增数据转换工具

---
+++ src/main/java/com/weaver/util/slf/DataConvertUtil.java
@@ -0,0 +1,67 @@
+package com.weaver.util.slf;
+
+import cn.hutool.core.util.NumberUtil;
+import cn.hutool.core.util.StrUtil;
+
+import java.math.BigDecimal;
+
+/**
+ * @author slf
+ * @date 2023/11/24
+ * 数据转换工具
+ */
+public class DataConvertUtil {
+    public static String ZERO = "0";
+
+    /**
+     * 非法字符串返回0字符串，用于合法化数值类型入参
+     * @param num 数值字符串
+     * @return 合法数值
+     */
+    public static String illegalStrToZero(String num) {
+        if (NumberUtil.isNumber(num)) {
+            return num;
+        }
+        return ZERO;
+    }
+
+    /**
+     * 字符串安全转换到BigDecimal
+     * @param num 数值字符串
+     * @return BigDecimal
+     */
+    public static BigDecimal strToBigDecimal(String num) {
+        try {
+            return new BigDecimal(num);
+        } catch (Exception e) {
+            return new BigDecimal(ZERO);
+        }
+    }
+
+    /**
+     * 比较是否为0
+     * @param b bigDecimal
+     * @return boolean
+     */
+    public static boolean equalsZero(BigDecimal b) {
+        return b.compareTo(BigDecimal.ZERO) == 0;
+    }
+
+    /**
+     * 比较是否为0，非法字符认为是0
+     * @param num num
+     * @return boolean
+     */
+    public static boolean equalsZero(String num) {
+        return equalsZero(strToBigDecimal(num));
+    }
+
+    /**
+     * 比较是否为0
+     * @param d double
+     * @return boolean
+     */
+    public static boolean equalsZero(double d) {
+        return equalsZero(BigDecimal.valueOf(d));
+    }
+}
