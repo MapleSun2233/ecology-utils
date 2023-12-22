34d9bb0dd7455b086ef70744576062f9f6a47007
maple <1326808745@qq.com>
2023-12-22 11:02:03 +0800

    [feat]新增定时器

---
+++ src/main/java/com/weaver/util/slf/TimerUtil.java
@@ -0,0 +1,42 @@
+package com.weaver.util.slf;
+
+import com.weaver.util.slf.interfaces.Callback;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * @author slf
+ * @date 2023/12/21
+ * 定时器工具
+ */
+public class TimerUtil {
+    /**
+     * 延迟时间后执行一次， 单位毫秒
+     * @param task 任务
+     * @param delay 延迟
+     */
+    public static void setTimeout(Callback task, long delay) {
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                task.run();
+            }
+        }, delay);
+    }
+
+    /**
+     * 延迟时间后执行第一次，间隔时间循环执行，单位毫秒
+     * @param task  任务
+     * @param delay 延迟
+     * @param interval 时间间隔
+     */
+    public static void setInterval(Callback task, long delay, long interval) {
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                task.run();
+            }
+        }, delay, interval);
+    }
+}
---
+++ src/main/java/com/weaver/util/slf/interfaces/Callback.java
@@ -0,0 +1,6 @@
+package com.weaver.util.slf.interfaces;
+
+@FunctionalInterface
+public interface Callback {
+    void run();
+}
