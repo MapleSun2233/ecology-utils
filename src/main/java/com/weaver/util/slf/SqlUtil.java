802bb94eacee85b3920bb079773755fe85198785
maple <1326808745@qq.com>
2024-10-08 14:08:33 +0800

    [fixed]校准sql注入检查关键词

--- src/main/java/com/weaver/util/slf/SqlUtil.java
+++ src/main/java/com/weaver/util/slf/SqlUtil.java
@@ -16,7 +16,7 @@ import java.util.Arrays;
  */
 public class SqlUtil {
     private static final BaseBean UTILS = new BaseBean();
-    private static final String[] SENSITIVE_WORDS = {";", " delete ", " update ", " into ", " drop ", " alter ", " union ", " join "};
+    private static final String[] SENSITIVE_WORDS = {";", " delete ", " update ", " into ", " drop ", " alter "};
     /**
      * 执行任意sql结果转jsonArr
      * @param sql sql
