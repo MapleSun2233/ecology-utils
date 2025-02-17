b4ce97740d1da0758c268cd4fa41bc011f32377f
maple <1326808745@qq.com>
2025-02-18 00:50:48 +0800

    [fixed]还原旧版本，解决本地EH缓存不过期的问题

--- src/main/java/com/weaver/util/slf/CacheUtil.java
+++ src/main/java/com/weaver/util/slf/CacheUtil.java
@@ -2,13 +2,18 @@ package com.weaver.util.slf;
 
 import com.cloudstore.dev.api.util.Util_DataCache;
 
+import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
+import java.util.Set;
+import java.util.concurrent.CopyOnWriteArraySet;
 
 /**
  * @author slf
  * @date 2023/9/14
  */
 public class CacheUtil {
+    private static final Set<String> KEYS = new CopyOnWriteArraySet<>();
+    private static final String TIMEOUT_SUFFIX = "-timeout";
     private static final String CACHE_PREFIX = "slf-";
 
     /**
@@ -17,29 +22,31 @@ public class CacheUtil {
      * @param value value
      */
     public static void set(String key, Object value) {
-        Util_DataCache.setObjVal(CACHE_PREFIX + key, value);
+        String cacheKey = CACHE_PREFIX + key;
+        Util_DataCache.setObjVal(cacheKey, value);
+        KEYS.add(key);
     }
 
     /**
-     * 废弃，已更换底层实现，时间单位不生效，timeout单位固定为秒
+     * 设置缓存，指定过期时间
      * @param key key
      * @param value value
      * @param timeout timeout
      * @param unit unit
      */
-    @Deprecated
     public static void set(String key, Object value, int timeout, ChronoUnit unit) {
-        Util_DataCache.setObjVal(CACHE_PREFIX + key, value, timeout);
+        set(key, value);
+        Util_DataCache.setObjVal(CACHE_PREFIX + key + TIMEOUT_SUFFIX, LocalDateTime.now().plus(timeout, unit));
     }
-
     /**
-     * 设置过期缓存
-     * @param key
-     * @param value
-     * @param seconds
+     * 设置缓存，指定过期时间
+     * @param key key
+     * @param value value
+     * @param timeout timeout
      */
-    public static void set(String key, Object value, int seconds) {
-        Util_DataCache.setObjVal(CACHE_PREFIX + key, value, seconds);
+    public static void set(String key, Object value, int timeout) {
+        set(key, value);
+        Util_DataCache.setObjVal(CACHE_PREFIX + key + TIMEOUT_SUFFIX, LocalDateTime.now().plus(timeout, ChronoUnit.SECONDS));
     }
 
     /**
@@ -48,7 +55,24 @@ public class CacheUtil {
      * @return bool
      */
     public static boolean contains(String key) {
-        return Util_DataCache.containsKey(CACHE_PREFIX + key);
+        String cacheKey = CACHE_PREFIX + key;
+        String timeoutKey = cacheKey + TIMEOUT_SUFFIX;
+        if (Util_DataCache.containsKey(cacheKey)) {
+            if (Util_DataCache.containsKey(timeoutKey)) {
+                LocalDateTime timeout = (LocalDateTime)Util_DataCache.getObjVal(timeoutKey);
+                if (LocalDateTime.now().isBefore(timeout)) {
+                    return true;
+                } else {
+                    clear(key);
+                    return false;
+                }
+            } else {
+                return true;
+            }
+        } else {
+            clear(key);
+            return false;
+        }
     }
 
     /**
@@ -59,7 +83,7 @@ public class CacheUtil {
      * @return T
      */
     public static <T> T get(String key, Class<T> t) {
-        Object obj = get(key);
+        Object obj = Util_DataCache.getObjVal(CACHE_PREFIX + key);
         if (t.isInstance(obj)) {
             return t.cast(obj);
         }
@@ -80,31 +104,31 @@ public class CacheUtil {
      * @param key key
      */
     public static void clear(String key) {
-        Util_DataCache.clearVal(CACHE_PREFIX + key);
+        String cacheKey = CACHE_PREFIX + key;
+        Util_DataCache.clearVal(cacheKey);
+        Util_DataCache.clearVal(cacheKey + TIMEOUT_SUFFIX);
+        KEYS.remove(key);
     }
 
     /**
-     * 废弃无效，清除超时缓存
+     * 清除超时缓存
      */
-    @Deprecated
     public static void clearTimeout() {
-//        KEYS.forEach(CacheUtil::contains);
+        KEYS.forEach(CacheUtil::contains);
     }
     /**
-     * 废弃无效，清除指定后缀的缓存
+     * 清除指定后缀的缓存
      * @param suffix suffix
      */
-    @Deprecated
     public static void clearContainSuffix(String suffix) {
-//        KEYS.stream().filter(key -> key.endsWith(suffix))
-//                .forEach(CacheUtil::clear);
+        KEYS.stream().filter(key -> key.endsWith(suffix))
+                .forEach(CacheUtil::clear);
     }
 
     /**
-     * 废弃无效,清除所有缓存
+     * 清除所有缓存
      */
-    @Deprecated
     public static void clearAll() {
-//        KEYS.forEach(CacheUtil::clear);
+        KEYS.forEach(CacheUtil::clear);
     }
 }
